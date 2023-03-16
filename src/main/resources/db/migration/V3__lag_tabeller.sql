1b066d871f94170b731f1b581ac0041e73522613
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-16 21:06:11 +0100

    Rename tabeller

--- src/main/resources/db/migration/V3__lag_tabeller.sql
+++ src/main/resources/db/migration/V3__lag_tabeller.sql
@@ -1,11 +1,11 @@
-CREATE TABLE payloads
+CREATE TABLE payload
 (
     id           VARCHAR DEFAULT uuid_generate_v4() PRIMARY KEY,
     frontend_app VARCHAR NOT NULL,
     opprettet    TIMESTAMP WITH TIME ZONE,
-    x_request_id VARCHAR NOT NULL,
-    json_payload VARCHAR NOT NULL
+    request_id   VARCHAR NOT NULL,
+    payload      VARCHAR NOT NULL
 );
 
-CREATE INDEX i_payloads_frontend_app_opprettet ON payloads (frontend_app, opprettet);
-CREATE INDEX i_payloads_x_request_id ON payloads (x_request_id);
+CREATE INDEX i_payload_frontend_app_opprettet ON payload (frontend_app, opprettet DESC);
+CREATE INDEX i_payload_request_id ON payload (request_id);
--- src/main/resources/db/migration/V4__slett_tabeller.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX i_payloads_frontend_app_opprettet;
-DROP INDEX i_payloads_x_request_id;
-DROP TABLE payloads;
